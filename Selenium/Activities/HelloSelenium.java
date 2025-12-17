188e82effe81b5bffea63f123a076fc42f2674c0
NehaKhare2409-FST <neha.khare2@ibm.com>
2025-12-17 08:59:13 +0530

    Add files via upload

---
+++ Selenium/Activities/HelloSelenium.java
@@ -0,0 +1,20 @@
+package Examples;
+
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+
+	public class HelloSelenium {
+	  public static void main(String[] args) {
+	   
+	    WebDriver driver = new FirefoxDriver();
+	    
+	
+	    driver.get("https://training-support.net");
+
+	    
+	    driver.quit();
+	  }
+	}
+
+
